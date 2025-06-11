6759a586a10d83e89ffc267317389b1b89f1e0bf
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-06-11 22:52:30 +0900

    'data_rollback'

--- 5-sprint-mission/src/main.ts
+++ 5-sprint-mission/src/main.ts
@@ -1,5 +1,7 @@
 import app from './app';
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const port = Number(process.env.SERVER_PORT) || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
