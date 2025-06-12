0174614378b29292ead7612549a0e0dd95e967ec
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-06-12 17:19:34 +0900

    'start.sh_change'

--- 5-sprint-mission/infra/ec2/start.sh
+++ 5-sprint-mission/infra/ec2/start.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 set -eux
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo apt-get update -y
-sudo apt-get install -y docker-compose
-
+echo "Docker 설치"
+sudo yum update -y
+sudo amazon-linux-extras enable docker
+sudo yum install -y docker
+sudo service docker start
 sudo usermod -aG docker $USER
+newgrp docker
 
-sudo apt-get install -y awscli
-sudo apt-get install -y awscli jq 
+echo "Docker Compose 설치" 
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.6/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose version
 
+echo "AWS CLI + jq 설치"
+sudo yum install -y awscli jq 
+
+echo "SSM 파리미터로 .env.production 생성"
 aws ssm get-parameters-by-path \
   --path "/MyApp/prod/" \
   --with-decryption \
@@ -21,5 +28,6 @@ cat /tmp/env.json \
   | jq -r '.[] | (.Name | split("/")[-1]) + "=" + .Value' \
   > .env.production
 
+echo "Docker Compose 실행"
 docker compose -f docker-compose.prod.yml down || true
 docker compose -f docker-compose.prod.yml up -d --build    
